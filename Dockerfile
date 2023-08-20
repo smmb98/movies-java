c0fcd2471791d789e3fdc5381586fd7ec014e055
S.M. Mohibullah Bukhari <mohibullahbukhari98@gmail.com>
2023-08-20 12:57:50 +0500

    Created MongoDB container, created Movie.java

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+#FROM openjdk:17
+#
+#WORKDIR /app
+#COPY . .
+#RUN mvn install
+#
+#CMD ["java", "-jar", "./target/movies-0.0.1-SNAPSHOT.jar"]
--- src/main/java/dev/mohib/movies/MoviesApplication.java
+++ src/main/java/dev/mohib/movies/MoviesApplication.java
@@ -1,13 +1,28 @@
 package dev.mohib.movies;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MoviesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoviesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Load environment variables from .env file
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty("server.port", dotenv.get("SERVER_PORT"));
+        System.setProperty("spring.data.mongodb.uri", dotenv.get("DATABASE_URL"));
+        System.setProperty("spring.data.mongodb.database", "movies-api-dbs");
+        SpringApplication.run(MoviesApplication.class, args);
+    }
+    
+    @GetMapping("/")
+    public String apiRoot() {
+        return "Hello World";
+    }
 
 }
